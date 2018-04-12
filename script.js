68f7e1feaaf02a7ce2dcb0b7b45d800c749ee24d
Aleksander Kwiatkowski <aleksander.kwiatkowski@gmail.com>
2018-04-12 19:57:27 +0200

    Add jQuery function inserting buttons onto page

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 $('span:even').css('color', 'red');
+
+var paragraphs = $('p');
+paragraphs.each(function(index, element) {
+	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+$(element).append(button);
+});
+
